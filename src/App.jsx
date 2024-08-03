c1cb338137edb155aeec54492f52ceb4fa95904d
Sahar <saharghasemi7711@gmail.com>
2024-08-03 23:31:58 +1000

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ function App() {
 
   return (
     <>
-     <div>dtart</div>
-    </>
+ <h1 className="text-3xl font-bold underline text-center mt-10">
+      Hello world!
+    </h1>    </>
   )
 }
 
