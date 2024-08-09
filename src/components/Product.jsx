c3f856ad018eb14d0dabcb0026959bd9a15c883b
Sahar <saharghasemi7711@gmail.com>
2024-08-09 13:19:40 +1000

    use LinesEllipsis to show 3 line of description in Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import { NumericFormat } from "react-number-format";
+import LinesEllipsis from "react-lines-ellipsis";
 
 const Product = ({ product }) => {
   const { id, title, description, sticker, price } = product;
-  const priceDollars=price/100;
+  const priceDollars = price / 100;
   return (
     <Link to={`products/${id}`}>
       <div className=" rounded shadow-lg mx-auto border border-light ">
@@ -14,14 +15,21 @@ const Product = ({ product }) => {
           <div className="text-primary text-lg pt-2 px-2 font-semibold">
             {title}
           </div>
-          <div className="text-base text-gray-600 p-1 mb-10 ">{description}</div>
+          <LinesEllipsis
+            className="text-base text-gray-600 p-1 mb-10 "
+            text={description}
+            maxLine="3"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
           <div className="absolute bottom-0 right-0 mb-2 pl-8 pr-4 pb-1 pt-2 bg-lighter triangle">
             <NumericFormat
               value={priceDollars}
               prefix={"$ "}
               displayType={"text"}
               thousandSeparator={true}
-              className="text-dark font-semibold  bg-lighter rounded-md px-4 py-1 "              
+              className="text-dark font-semibold  bg-lighter rounded-md px-4 py-1 "
             />
           </div>
         </div>
